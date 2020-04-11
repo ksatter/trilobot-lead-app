707ebb1d26c71f72dc8e10ae9ed8403dec514d4e
Katheryn Satterlee <me@ksatter.com>
2020-04-11 11:31:54 -0500

    initial setup complete. Able to log in and view leads.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const oAuthRoutes = require('./oauth');
 
 const router = require('express').Router();
 
+router.get('*', (req, res) => {
+    res.send("Nothing to see here, folks. Move it along.")
+})
+
 router.use('/api', apiRoutes);
 router.use('/oauth', oAuthRoutes);
 
