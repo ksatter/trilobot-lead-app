a5120970ef16d6159f86285dfa452fe54b67c5a0
Katheryn Satterlee <me@ksatter.com>
2020-04-29 12:15:06 -0500

    fixing oauth routes

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const oAuthRoutes = require('./oauth');
 
 const router = require('express').Router();
 
-router.get('*', (req, res) => {
-    res.send("Nothing to see here, folks. Move it along.")
-})
+
 
 router.use('/api', apiRoutes);
 router.use('/oauth', oAuthRoutes);
 
+router.get('*', (req, res) => {
+    res.send("Nothing to see here, folks. Move it along.")
+})
 
 module.exports = router
