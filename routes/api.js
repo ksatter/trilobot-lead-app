db5aa72c3a7fc16f21ede342bae8236f8a1f5812
Katheryn Satterlee <me@ksatter.com>
2020-04-29 21:36:49 -0500

    adding routes for cron-job.org to prevent server idling and auto-clear

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const {buildMessage} = require('../lib/store')
+const {buildMessage, cronClear} = require('../lib/store')
 
-
+//BOT ROUTES
 
 router.route('/leads')
     .post((req, res) => {
@@ -12,8 +12,20 @@ router.route('/leads')
             .catch(message => res.status(200).send(message))
     })
 
-router.route('/sayhi')
+//CRON ROUTES (cron-job.org)
+
+//Server is pinged every 30 minutes from 9am to 12am CST to avoid idling in heroku.
+router.route('/cron/sayhi')
     .get((req, res) => {
         res.send('Hello')
     })
+
+//endpoint for auto-clear of lead list. Pinged daily at 1AM CST.
+router.route('/cron/clear')
+    .post((req, res) => {
+        cronClear(req.body)
+            .then(message => res.send(message))
+    })
+
+    
 module.exports = router
